3c92d73aba01d9586e48c1834fce2b9429b6ff0a
Vic Chang <vicchang@12cm.com.tw>
2024-04-07 19:49:43 +0800

    add security config

--- src/main/java/com/lazyvic/tacos/security/SecurityConfig.java
+++ src/main/java/com/lazyvic/tacos/security/SecurityConfig.java
@@ -1,4 +1,24 @@
 package com.lazyvic.tacos.security;
 
+import com.lazyvic.tacos.User;
+import com.lazyvic.tacos.data.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+        return username -> {
+            User user = userRepo.findByUsername(username);
+            if (user != null) {
+                return user;
+            }
+
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        };
+    }
 }
